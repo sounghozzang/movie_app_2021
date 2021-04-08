f55356b5260ffb4a8e66ad823d75caddf6e9259a
soungho_cho <soungho_cho@tmax.co.kr>
2021-04-08 12:54:44 +0900

    Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -2,8 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 
-function Movie({id, year, title, summary, poster}){
-    return <h1>{title}</h1>
+function Movie({year, title, summary, poster}){
+    return <div class="movie">
+        <img src={poster} alt={title} title={title}></img>
+        <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="movie_summary">{summary}</p>
+        </div>
+    </div>
 }
 
 Movie.propTypes = {
